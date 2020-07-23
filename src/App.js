e2d0abe36a42a50f902f5427c040bc426d866e5e
alhaqhassan <alhaqhassan@gmail.com>
2020-07-23 20:41:00 +0530

    added modules

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Main from './main';
+function App() {
+  return (
+      <Main />
+  );
+}
+export default App;
+ 
+
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React, {Suspense,useState,useEffect } from 'react';
+import {Route, Switch, withRouter,useHistory} from 'react-router-dom';
+const Main = () => {
+  return (
+  	<div>
+  	
+    </div>
+    
+  );
+};
+
+export default Main;
+
+
+
+
